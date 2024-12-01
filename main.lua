1b50c2d79278b5162437d8ee3aa5349feb13462c
Taiga <combiasso57@gmail.com>
2024-12-02 00:00:18 +0100

    Virtual Joystick implementation, les boolens c'est la vie

--- main.lua
+++ main.lua
@@ -6,28 +6,32 @@ function love.load()
   r = 1
   g = 0
   r_setup = 50
+  token = 0
 end
 function love.update(dt)
   local t = love.touch.getTouches()
-  
     for i, id in ipairs(t) do
         local touchX, touchY = love.touch.getPosition(id)
-      if (touchX <= 300) then
+      if (touchX < 200) then --handles the joystick space
           if (touchX <= holderX + (r_setup/2 + 20) ) and (touchX >= holderX - (r_setup/2 +20)) and (touchY <= holderY + (r_setup/2 +20)) and (touchY >= holderY - (r_setup/2 +20)) then
+              token = 1
               holderX = touchX
               holderY = touchY
           end
+        elseif (token == 1) then
+              holderY = touchY
       end
     end
+  end
   function love.touchpressed(id,x,y,dx,dy, pressure)
         printed = count[2]
   end
   function love.touchreleased(id,x,y,dx,dy, pressure)
     printed = count[1]
-    holderX = 200
+    token = 0
+    holderX = 150
     holderY = 200
   end
-end
 function love.draw()
   love.graphics.setColor(1, 1, 1)
   love.graphics.circle("fill", holderX, holderY, r_setup, 100)
